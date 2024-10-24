eccd65b0d7be6ed3fba566309d8bc79d1e187f7a
Saitos02 <gerd02@hotmail.com>
2024-10-24 12:59:44 +0200

    Menu
    Skonczone menu. Poczatek przejscia miedzy poziomami.

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -1,21 +1,29 @@
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void StartGame()
     {
-        
+        SceneManager.LoadSceneAsync("LVL1", LoadSceneMode.Single);
     }
 
     // Update is called once per frame
-    public void Options()
+    public GameObject OptionsPanel;
+    public void Options(bool isActive)
     {
-        
+        OptionsPanel.SetActive(isActive);   
+    }
+    public void OptionsExit(bool isActive)
+    {
+        OptionsPanel.SetActive(isActive);
     }
 
     public void Exit()
     {
-
+        Application.Quit();
     }
 }
