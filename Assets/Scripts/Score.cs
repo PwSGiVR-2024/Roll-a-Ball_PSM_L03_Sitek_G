f3924f7e4498815b209c84f431ba7168c0533079
Saitos02 <gerd02@hotmail.com>
2024-11-28 13:17:46 +0100

    proby, oswietlenie

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -11,26 +11,19 @@ public class CollectScore : MonoBehaviour
     
     public TMP_Text scoreText;
     public TMP_Text LVL_Pass;
-    public TMP_Text END_GAME;
     public MovementController player;
-    //public object scoreText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (player.score == 9)
+        if (player?.score == 9)
         {
             LVL_Pass.text = "Przeszedles pierwszy poziom! Przytrzymaj spacje aby skoczyc.";
-        }   
-        if(player.score == 18)
-        {
-            END_GAME.text = "Zdobyles wszystkie punkty! Wygrales!";
         }
     }
    
