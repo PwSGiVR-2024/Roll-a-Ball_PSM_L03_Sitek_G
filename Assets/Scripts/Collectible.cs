aeb7b4b88872345e1e18f706b502e34bb38012df
Saitos02 <gerd02@hotmail.com>
2025-01-24 17:34:16 +0100

    FINAL

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -2,20 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 using System;
 
 public class Collectible : MonoBehaviour
 {
     public event Action pickUp;
     public AudioSource m_AudioSource;
-    // Start is called before the first frame update
     void Start()
     {
         m_AudioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate(50*Time.deltaTime, 30 * Time.deltaTime, 30 * Time.deltaTime);
